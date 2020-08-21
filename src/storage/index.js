47ba6b526e2fbbe1e17c968f8da9c7f10faefeb8
rsj2003 <rsj2003@naver.com>
2020-08-21 14:16:55 +0900

    refactor: Toggle Method 제거

--- src/storage/index.js
+++ src/storage/index.js
@@ -18,19 +18,4 @@ export default Object.freeze({
         localStorage.setItem(key, JSON.stringify(value));
     },
 
-    /**
-     * key에 value를 할당하고, toogle 시킴
-     * @param key {string}
-     * @param value {string}
-     * @param toggle {'t'|'f'}
-     */
-    toggle(key, value, toggle) {
-        if (this.get(key) === null) {
-            this.set(key, [value, toggle]);
-            return;
-        }
-        const before = this.get(key).split(',')[0];
-        this.set(key, [value, before === 't' ? 'f' : 't']);
-    },
-
 });
